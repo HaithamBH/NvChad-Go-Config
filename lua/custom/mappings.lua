505f357be041b199ec6cfb60d0319f6407729375
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-03 07:21:39 +0100

    golang debugger and tools

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -6,8 +6,44 @@ M.general = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
+}
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
 }
 
 -- more keybinds!
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -36,18 +36,17 @@ local plugins = {
   {
     "mfussenegger/nvim-dap",
     init = function()
-      require("core.utils").load_mappings "dap"
-    end,
+      require("core.utils").load_mappings("dap")
+    end
   },
-
   {
-    "leoluz/nvim-dap-go",
+    "dreamsofcode-io/nvim-dap-go",
     ft = "go",
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
       require("dap-go").setup(opts)
-      require("core.utils").load_mappings "dap_go"
-    end,
+      require("core.utils").load_mappings("dap_go")
+    end
   },
 
   {
@@ -60,6 +59,17 @@ local plugins = {
     ft = "go",
     opts = function()
       return require "custom.configs.null-ls"
+    end,
+  },
+
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
     end,
   },
 
