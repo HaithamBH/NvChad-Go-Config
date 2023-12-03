4e1e5c2ae82231ab3c6c70154640ff3bf46bef3e
HaithamBH <haitham.benhammouda@gmail.com>
2023-12-03 21:17:34 +0100

    edit mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,9 +4,22 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["df"] = {
+      function()
+        vim.lsp.buf.definition()
+      end,
+      "go to definition",
+      opts = { nowait = true },
+    },
+    ["<A-j>"] = { ":m .+1<CR>==", "move line down" },
+    ["<A-k>"] = { ":m .-2<CR>==", "move line up" },
   },
   v = {
     [">"] = { ">gv", "indent" },
+  },
+  i = {
+    ["<C-o>"] = { "<ESC>o", "return to line above" },
+    ["<C-O>"] = { "<ESC>O", "return to line below" },
   },
 }
 
@@ -15,17 +28,17 @@ M.dap = {
   n = {
     ["<leader>db"] = {
       "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line"
+      "Add breakpoint at line",
     },
     ["<leader>dus"] = {
-      function ()
-        local widgets = require('dap.ui.widgets');
-        local sidebar = widgets.sidebar(widgets.scopes);
-        sidebar.open();
+      function()
+        local widgets = require "dap.ui.widgets"
+        local sidebar = widgets.sidebar(widgets.scopes)
+        sidebar.open()
       end,
-      "Open debugging sidebar"
-    }
-  }
+      "Open debugging sidebar",
+    },
+  },
 }
 
 M.dap_go = {
@@ -33,17 +46,17 @@ M.dap_go = {
   n = {
     ["<leader>dgt"] = {
       function()
-        require('dap-go').debug_test()
+        require("dap-go").debug_test()
       end,
-      "Debug go test"
+      "Debug go test",
     },
     ["<leader>dgl"] = {
       function()
-        require('dap-go').debug_last()
+        require("dap-go").debug_last()
       end,
-      "Debug last go test"
-    }
-  }
+      "Debug last go test",
+    },
+  },
 }
 
 -- more keybinds!
